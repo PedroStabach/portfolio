fd4845a93841d691c0ea05fd67f2831fd5fda48a
Pedro Stabach <pedroaugustostabach3@gmail.com>
2025-05-04 23:41:35 -0300

    pronto para dar deploy

--- app.js
+++ app.js
@@ -79,3 +79,9 @@ async function todosProjetos() {
 
 }
 todosProjetos();
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("mobile-menu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
