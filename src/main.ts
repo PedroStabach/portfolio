d271df6c15414d0671384d9ee684e85eaa482b76
Pedro Stabach <pedroaugustostabach3@gmail.com>
2025-05-05 00:38:37 -0300

    serve index.html na rota principal

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Use o valor de process.env.PORT ou 3000 se não estiver definido
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
